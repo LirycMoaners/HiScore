a7e04412c9d740d7ce34520f1b5c892b1180f70e
Cyril <cyril.lefebvre@soprasteria.com>
2020-04-07 17:06:03 +0200

    feat(Help & About) Update infos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,5 +14,11 @@ export const environment = {
     messagingSenderId: '299295266645',
     appId: '1:299295266645:web:552dc53389e6bd48a201dc',
     measurementId: 'G-MY486HKPP2'
+  },
+  links: {
+    googlePlay: 'https://play.google.com/store/apps/details?id=com.cyrillefebvre.hiscore',
+    hiScoreWeb: 'https://hi-score-app.wep.app',
+    linkedIn: 'https://www.linkedin.com/in/cyril-lefebvre-2a818294/',
+    github: 'https://github.com/LirycMoaners'
   }
 };
